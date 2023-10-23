73251ba3f69c10145b67fe7b8810a67f13030dba
Petro Svist <svist.petro@gmail.com>
2023-10-23 22:12:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import user from '../data/user.json';
-import data from '../data/data.json';
+// import data from '../data/data.json';
 
 export const App = () => {
   return (
