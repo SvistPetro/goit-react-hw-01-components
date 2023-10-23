6dccdae519b83c80b6c9fd7d6c0461a79a94778e
Petro Svist <svist.petro@gmail.com>
2023-10-23 22:01:03 +0300

    Task 1 not css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template, Petro!
+      <Profile 
+        avatar={user.avatar}
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+        />
+
+      <Statistics />
     </div>
   );
 };
