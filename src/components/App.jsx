79971f3e80c7f927a436e628404aa325a8d48282
Petro Svist <svist.petro@gmail.com>
2023-10-25 21:46:13 +0300

    Task 4 not styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import FriendListItem from './FriendListItem/FriendListItem'
+import FriendListItem from './FriendListItem/FriendListItem';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -19,6 +22,8 @@ export const App = () => {
       <Statistics stats={data}/>
 
       <FriendListItem friends={friends}/>
+
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
