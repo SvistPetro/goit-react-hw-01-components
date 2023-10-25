0f6c55d85100036bc464bde2473de7440d1d93b4
Petro Svist <svist.petro@gmail.com>
2023-10-25 23:19:50 +0300

    поправка Таск 2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ const FriendListItem = (props) => {
         <ul className="friend-list">
             {props.friends.map(friend => {
                 return <li key={friend.id} className="item">
-                            {friend.isOnline ? <span className="status isOnline">123</span> : <span className="status isOfline">123</span>}
+                            {friend.isOnline ? <span className="status isOnline"></span> : <span className="status isOfline">123</span>}
                             <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
                             <p className="name">{friend.name}</p>
                         </li>
