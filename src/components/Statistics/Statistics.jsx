f373ff1861d75c6d706d96687c4ba2a03d8be91b
Petro Svist <svist.petro@gmail.com>
2023-10-26 21:13:48 +0300

    Task 2 with Styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,14 @@
+import css from './Statistics.module.css';
+
 const Statistics = (props) => {
     return (
-        <section className="statistics">
-            {props.title ? <h2 className="title">{props.title}</h2> : <h2 className="title">Upload stats</h2>}
+        <section className={css.statistics}>
+            {props.title ? <h2 className={css.title}>{props.title}</h2> : <h2 className={css.title}>UPLOAD STATS</h2>}
 
-            <ul className="stat-list">
+            <ul className={css.list}>
 
             {props.stats.map(stats => {
-                return <li key={stats.id} className="item"><span className="label">{stats.label}</span><span className="percentage">{stats.percentage}</span>
+                return <li key={stats.id} className={css.item}><span className={css.label}>{stats.label}</span><span className={css.percentage}>{stats.percentage}%</span>
             </li>
             })}
             </ul>
