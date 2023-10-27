f4c292e741fd443dd1957c4a4eaf68a14b7f4370
Petro Svist <svist.petro@gmail.com>
2023-10-27 21:09:07 +0300

    Task 3 with Styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,7 +5,7 @@ const FriendListItem = (props) => {
         <ul className={css.list}>
             {props.friends.map(friend => {
                 return <li key={friend.id} className={css.item}>
-                            {friend.isOnline ? <span className={css.isOnline}></span> : <span className={css.isOfline}></span>}
+                            {friend.isOnline ? <span className={css.isOnline}></span> : <span className={css.isOffline}></span>}
                             <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
                             <p className={css.name}>{friend.name}</p>
                         </li>
